e92737201437410d6533d17cfe9335de3d5cbf29
Michael Brown <michael.brown1@uky.edu>
2014-06-10 11:47:46 -0400

    update to DB addition code

---
+++ MWPCecalDB.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python                                                               
+
+import sys
+import os
+sys.path.append(os.environ("G4BINDIR")+"../../Scripts")
+from ucnacore.EncalDB import *
+
+class WirechamberDBfill(run, side, anORcath, ):
+    def __init__(self):
+        self.
