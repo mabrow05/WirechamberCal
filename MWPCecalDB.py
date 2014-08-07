f1b2f65fbfc459464380140bf4428a74aeadb67a
Michael Brown <michael.brown1@uky.edu>
2014-08-07 17:37:11 -0400

    Adding ability to upload posmaps

--- MWPCecalDB.py
+++ MWPCecalDB.py
@@ -2,6 +2,7 @@
 
 import sys
 import os
+import numpy
 sys.path.append(os.environ("G4BINDIR")+"../../Scripts")
 from ucnacore.EncalDB import *
 
@@ -82,12 +83,77 @@ class WirechamberDBfill(anORcath="cathode"):
         print "Run %i loaded to database"%(run)
         
 
-    def LoadMap(self, conn):
-        hold
+    def LoadMap(self, conn, nrings, rmin, rmax, emin, emax ):
+        radius=50
+        descrip="%s %i %i"%(charge_meas, rmin, rmax)
+        cmd="INSERT INTO posmap_set (descrip, n_rings, radius) "
+        cmd+="VALUES ('%s',%i,%i)"%(descrip,nrings,radius)
+        print cmd
+        #conn.execute(cmd)
+        #conn.execute("SELECT LAST_INSERT_ID()")
+        #pmid=int(conn.fetchone()[0])
 
+        
+        for side in ["east", "west"]:
+            if side=="east":
+                Side="East"
+            else:
+                Side="West"
+                
+            mapval=[]
+            pixelID=[]
+            x0=[]
+            y0=[]
+            rmin=None
+            rmax=None
+            phimin=None
+            phimax=None
+            Side=None
+
+            fin="%irings_%i-%ikeV_posMap_%i-%i_%s_%s.dat"%(nrings, emin, emax, rmin, rmax,self.anORcath, side) 
+            infile = open(fin, "r")
+            pid=0
+            for line in infile:
+                vals=line.split()
+                rmin=float(vals[0])
+                rmax=float(vals[1])
+                phimin=float(vals[2])
+                phimax=float(vals[3])
+                m=float(vals[4])
+                pixelID.append(pid)
+                pid=pid+1
+
+                if rmin<0.001:
+                    x0.append(0.)
+                    y0.append(0.)
+                    mapval.append(m)
+                    
+                else:
+                    r = (rmax-rmin)/2.
+                    phi = (phimax-phimin)/2.
+                    x = r*numpy.cos(phi)
+                    y = r*numpy.sin(phi)
+                    x0.append(x)
+                    y0.append(y)
+                    mapval.append(m)
+
+            for i in range(0, len(pixelID), 1):
+                print pixelID[i], x0[i], y0[i], mapval[i]
+                
+                #cmd2="INSERT INTO posmap_points (posmap_set_id,side,quadrant,pixel_id,signal,center_x,center_y,norm) "
+                #cmd2+="VALUES (%i,'%s',0,%i,%f,%f,%f,1)"%(pmid,Side,pixelID[i],mapval[i],x0[i],y0[i])
+                #print cmd2
+                #conn.execute(cmd2)
 
 if __name__ == "__main__":
 
-	conn = open_connection()
+    nrings=6
+    rmin=18081
+    rmax=18090
+    emin=400
+    emax=600
 
+    conn = open_connection()
+    db=WirechamberDBfill()
+    db.LoadMaps(conn,nrings,rmin,rmax,emin,emax)
 
